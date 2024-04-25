2cdafa826916fbf5e9cede923107c7bccb1510ca
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2024-04-25 15:46:50 -0400

    feat(login-verify): decouple visiting /login-verify from retrieving the credentials, add button to retrieve credentials with user interaction

--- src/routes/user/login-verify/+page.server.ts
+++ src/routes/user/login-verify/+page.server.ts
@@ -1,26 +1,27 @@
 import { fail } from '@sveltejs/kit';
 import { CONFIG } from '../../../config/config.js';
 
-/** @type {import('./$types').PageLoad} */
-export async function load({ url }) {
-    // retrieve verification key search param from URL
-    const verificationKey = url.searchParams.get('verification_key');
-    const options = {
-        method: 'POST',
-    }
+export const actions = {
+    default: async ({ url }) => {
+        const verificationKey = url.searchParams.get('verification_key');
+        console.log("verificationKey", verificationKey)
+        const options = {
+            method: 'POST',
+        }
 
-    // trade verification key for api key
-    let response;
-    try {
-        response = await fetch(`${CONFIG.API_URL}/api/v1/across/user/login-verify/${verificationKey}`, options)
-    } catch (error) {
-        console.log(`ERROR: login-verify for verificationKey [${verificationKey}] at [${Date.now()}]`, JSON.stringify(error))
-        return fail(500, { error: error?.message, fail: true });
-    }
+        // trade verification key for api key
+        let response;
+        try {
+            response = await fetch(`${CONFIG.API_URL}/api/v1/across/user/login-verify/${verificationKey}`, options)
+        } catch (error) {
+            console.log(`ERROR: login-verify for verificationKey [${verificationKey}] at [${Date.now()}]`, JSON.stringify(error))
+            return fail(500, { error: error?.message, fail: true });
+        }
 
-    const credentials = await response?.json();
+        const credentials = await response?.json();
 
-    return {
-        credentials
+        return {
+            credentials
+        }
     }
 }

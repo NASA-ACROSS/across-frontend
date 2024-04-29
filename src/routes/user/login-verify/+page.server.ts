0bfadf643cefcf2cedf083b8c4ae76b7fdbb7c33
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2024-04-29 12:42:15 -0400

    feat(login): add user login and login-verify routes (#35)
    * feat(user): added login and login-verify routes
    
    * fix(scripts): fix scripts not reloading on navigation in main layout, extract Footer and BackToTopButton as components, remove footer and back to top button from every page, add custom error page
    
    * feat(version): bump version to 0.0.4
    
    * feat(workflow): add manual ref override for selecting what to containerize to deploy-to-aws workflow
    
    * fix(imports): remove unused imports in main layout
    
    * fix(paths): enable relative paths to fix navigation issue #36
    
    * feat(login-verify): decouple visiting /login-verify from retrieving the credentials, add button to retrieve credentials with user interaction
    
    * fix(login): add required to email field
    
    * feat(error-handling): additional error handling for login-verify and login routes

---
+++ src/routes/user/login-verify/+page.server.ts
@@ -0,0 +1,33 @@
+import { fail } from '@sveltejs/kit';
+import { CONFIG } from '../../../config/config.js';
+
+export const actions = {
+    default: async ({ url }) => {
+        const verificationKey = url.searchParams.get('verification_key');
+
+        const options = {
+            method: 'POST',
+        }
+
+        // trade verification key for api key
+        let response;
+        try {
+            response = await fetch(`${CONFIG.API_URL}/api/v1/across/user/login-verify/${verificationKey}`, options)
+        } catch (error) {
+            console.log(`ERROR: login-verify for verificationKey [${verificationKey}] at [${Date.now()}]`, JSON.stringify(error))
+            return fail(500, { error: error?.message, fail: true });
+        }
+
+        // short circuit for error status
+        if (response.status != 200) {
+            console.log(`ERROR: login-verify for verificationKey [${verificationKey}] at time [${Date.now()}] with status code [${response.status}] with status text ${response.statusText}`, response)
+            return {}
+        }
+
+        const credentials = await response?.json();
+
+        return {
+            credentials
+        }
+    }
+}
---
+++ src/routes/user/login-verify/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+
+    import type { ActionData } from "./$types";
+    export let form;
+</script>
+
+<section class="py-5 bg-secondary">
+    <div class="container py-md-3">
+        {#if !form}
+            <form method="post">
+                <h1>Email Login</h1>
+                <button class="btn btn-lg btn-primary mb-3"
+                    >Show Credentials</button
+                >
+            </form>
+        {:else if form?.credentials?.api_token}
+            <h1>Email Login Success</h1>
+            <h2>Email: {form.credentials.email}</h2>
+            <h2>API Key: {form.credentials.api_token}</h2>
+            <p class="form-text fs-sm text-sm-start text-center">
+                Please store this API key in a secure location. You will not be
+                able to retrieve it again using this link.
+            </p>
+        {:else}
+            <h1>Email Login Error</h1>
+            <h4>Invalid ACROSS user verification key</h4>
+            <h4>
+                <a href="{base}/user/login" class=".text-decoration"
+                    >>Visit login to try again</a
+                >
+            </h4>
+        {/if}
+    </div>
+</section>

2f7c9fd7f15357b4648fe4784b996fe031675c85
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2024-05-13 16:30:55 -0400

    feat(limit): add rate limiter with retryAfter information to /login and /login-verify routes

--- src/routes/user/login-verify/+page.server.ts
+++ src/routes/user/login-verify/+page.server.ts
@@ -1,9 +1,30 @@
 import { fail } from '@sveltejs/kit';
 import { CONFIG } from '../../../config/config.js';
+import { RetryAfterRateLimiter } from 'sveltekit-rate-limiter/server';
+
+// rate limit is defined as [number, unit]
+// see documentation for more info
+// https://github.com/ciscoheat/sveltekit-rate-limiter?tab=readme-ov-file#valid-units
+const limiter = new RetryAfterRateLimiter({
+    // IP + User Agent limiter, 5 login requests per 15 mins, resetting every 15 minutes
+    IPUA: [5, '15m'], 
+    // IP address limiter, triple the limit to ensure multiple users from the same IP don't become limited
+    IP: [15, '15m'],
+});
 
 export const actions = {
-    default: async ({ url }) => {
+    default: async (event) => {
+        const { url } = event;
         const verificationKey = url.searchParams.get('verification_key');
+
+        // Rate limit user login-verify
+        // Every call to isLimited counts as a hit towards the rate limit for the event.
+        const rateStatus = await limiter.check(event);
+        if (rateStatus.limited){
+            console.error(`ERROR: rate-limiting at /login-verify for verificationKey [${verificationKey}] at time [${Date.now()}] with IP [${event.getClientAddress()}] with retryAfter [${rateStatus.retryAfter}] seconds`)
+            return fail(429, { rateLimit: true, retryAfter: rateStatus.retryAfter });
+        } 
+
 
         const options = {
             method: 'POST',
@@ -18,13 +39,13 @@ export const actions = {
         try {
             response = await fetch(`${CONFIG.API_URL}/api/v1/across/user/login-verify/${verificationKey}`, options)
         } catch (error: any) {
-            console.log(`ERROR: login-verify for verificationKey [${verificationKey}] at [${Date.now()}]`, JSON.stringify(error))
+            console.error(`ERROR: login-verify for verificationKey [${verificationKey}] at [${Date.now()}]`, JSON.stringify(error))
             return fail(500, { error: error?.message, fail: true });
         }
 
         // short circuit for error status
         if (response.status != 200) {
-            console.log(`ERROR: login-verify for verificationKey [${verificationKey}] at time [${Date.now()}] with status code [${response.status}] with status text ${response.statusText}`, response)
+            console.error(`ERROR: login-verify for verificationKey [${verificationKey}] at time [${Date.now()}] with status code [${response.status}] with status text ${response.statusText}`, response)
             return {}
         }
 

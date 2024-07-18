9f65ee1f24ab3e824dacc01c102c9031466ab0f9
Dan Kocevski <dankocevski@gmail.com>
2024-07-18 16:17:59 -0400

    Implemented nav menu proposal 3

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -76,119 +76,79 @@
                             <li>
                                 <a
                                     href="{base}/missions#development"
-                                    class="dropdown-item">Development</a
+                                    class="dropdown-item">In Development</a
                                 >
                             </li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
-                        <a
-                            href="{base}/tools"
-                            class="nav-link dropdown-toggle"
-                            aria-current="page">Tools</a
-                        >
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a
-                                    href="{base}/tools#overview"
-                                    class="dropdown-item">Overview</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/tools#reporting"
-                                    class="dropdown-item"
-                                    >Rapid Reporting Resources</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/tools#analysis"
-                                    class="dropdown-item"
-                                    >Data Analysis Toolkits</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/tools#planning"
-                                    class="dropdown-item"
-                                    >Observations Planning Tools</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/tools#archives"
-                                    class="dropdown-item"
-                                    >Data Archives & Repositories</a
-                                >
-                            </li>
-                        </ul>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">For Scientists</a>
+                        <div class="dropdown-menu">
+                            <div class="d-lg-flex pt-lg-3">
+                                <div class="mega-dropdown-column" style="width: 250px;">
+                                    <h6 class="px-3 mb-2"><a href="#">ACROSS Tools</a></h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="#" class="dropdown-item py-1">Mission Status Table</a></li>
+                                    <li><a href="#" class="dropdown-item py-1">Mission Skymap</a></li>
+                                    <li><a href="#" class="dropdown-item py-1">Mission ToO Pages</a></li>
+                                    <li><a href="#" class="dropdown-item py-1">ACROSS API</a></li>
+                                    </ul> 
+                                    <h6 class="px-3 mb-2"><a href="#">ACROSS Resources</a></h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="services-v1.html" class="dropdown-item py-1">TDAMM Help Desk</a></li>
+                                    <li><a href="#" class="dropdown-item py-1">Analysis Tutorials</a></li>
+                                    </ul>                                                                       
+                                    <h6 class="px-3 mb-2">Proposals</h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="blog-list-with-sidebar.html" class="dropdown-item py-1">ACROSS Community Grant Program</a></li>
+                                    <li><a href="blog-list-with-sidebar.html" class="dropdown-item py-1">NASA Funding Opportunities</a></li>
+                                    <li><a href="blog-grid-with-sidebar.html" class="dropdown-item py-1">Guest Investigator Programs</a></li>
+                                    <li><a href="blog-list-no-sidebar.html" class="dropdown-item py-1">Joint Observing Capabilities</a></li>
+                                    <li><a href="blog-grid-no-sidebar.html" class="dropdown-item py-1">Complementary Opportunities</a></li>
+                                    </ul>
+                                </div>
+                                <div class="mega-dropdown-column" style="width: 250px;">
+                                    <h6 class="px-3 mb-2">Community Resources</h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="tools" class="dropdown-item py-1">Rapid Alerts</a></li>
+                                    <li><a href="tools" class="dropdown-item py-1">Observation Planning Tools</a></li>
+                                    <li><a href="tools" class="dropdown-item py-1">Data Archives</a></li>
+                                    <li><a href="tools" class="dropdown-item py-1">Data Analysis Toolkits</a></li>
+                                    </ul>                                    
+                                    <h6 class="px-3 mb-2">Conferences</h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="portfolio-grid.html" class="dropdown-item py-1">ACROSS Workshops</a></li>
+                                    <li><a href="portfolio-grid.html" class="dropdown-item py-1">Upcoming Meetings</a></li>
+                                    <li><a href="portfolio-list.html" class="dropdown-item py-1">Past Meetings</a></li>
+                                    <li><a href="portfolio-slider.html" class="dropdown-item py-1">Presentation Resources</a></li>
+                                    </ul>
+                                    <h6 class="px-3 mb-2">Papers and Studies</h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="services-v1.html" class="dropdown-item py-1">White Papers</a></li>
+                                    <li><a href="services-v2.html" class="dropdown-item py-1">SIG Reports</a></li>
+                                    </ul>                                    
+                                </div>
+                                <!-- <div class="mega-dropdown-column">
+                                    <h6 class="px-3 mb-2">Pricing</h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="pricing.html" class="dropdown-item py-1">Pricing Page</a></li>
+                                    </ul>
+                                    <h6 class="px-3 mb-2">Contacts</h6>
+                                    <ul class="list-unstyled mb-3">
+                                    <li><a href="contacts-v1.html" class="dropdown-item py-1">Contacts v.1</a></li>
+                                    <li><a href="contacts-v2.html" class="dropdown-item py-1">Contacts v.2</a></li>
+                                    <li><a href="contacts-v3.html" class="dropdown-item py-1">Contacts v.3</a></li>
+                                    </ul>
+                                    <h6 class="px-3 mb-2">Specialty</h6>
+                                    <ul class="list-unstyled">
+                                    <li><a href="404-v1.html" class="dropdown-item py-1">404 Error v.1</a></li>
+                                    <li><a href="404-v2.html" class="dropdown-item py-1">404 Error v.2</a></li>
+                                    </ul>
+                                </div> -->
+                            </div>
+                        </div>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a
-                            href="{base}/proposals"
-                            class="nav-link dropdown-toggle"
-                            aria-current="page">Proposals</a
-                        >
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a
-                                    href="{base}/proposals#proposals"
-                                    class="dropdown-item"
-                                    >Proposal Opportunities</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/proposals#programs"
-                                    class="dropdown-item"
-                                    >Guest Observer & Investigator Programs</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/proposals#joint"
-                                    class="dropdown-item"
-                                    >Joint Observing Opportunities</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/proposals#resources"
-                                    class="dropdown-item"
-                                    >Complementary Opportunities</a
-                                >
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a
-                            href="{base}/conferences"
-                            class="nav-link dropdown-toggle"
-                            aria-current="page">Conferences</a
-                        >
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a
-                                    href="{base}/conferences#upcoming"
-                                    class="dropdown-item">Upcoming Meetings</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/conferences#past"
-                                    class="dropdown-item">Past Meetings</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="{base}/conferences#resources"
-                                    class="dropdown-item"
-                                    >Presentation Resources</a
-                                >
-                            </li>
-                        </ul>
-                    </li>
+                    
                     <li class="nav-item">
                         <a
                             data-sveltekit-reload

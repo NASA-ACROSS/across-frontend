298a40a2fb54fb282725fbb9cb38245f864a7825
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2024-08-20 15:03:35 -0400

    xfeat(user): refactor user request roles into user info, delete user requesting roles feature

---
+++ src/lib/types/User.ts
@@ -0,0 +1,24 @@
+type UserGroup = {
+    id: number;
+    name: string;
+    is_admin: boolean;
+    roles: UserGroupRoles[];
+};
+
+type UserGroupRoles = {
+    id: number;
+    name: string;
+    user_group_id: number;
+};
+
+export type User = {
+    id: number;
+    firstname: string;
+    lastname: string;
+    username: string;
+    email: string;
+    api_token: string;
+    rememberMe: boolean;
+    message: string;
+    user_groups: UserGroup[];
+};

6ebc9feae12ecb0906c304f0b4c7bcfd9c22dcba
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2024-08-20 17:21:24 -0400

    feat(usergroup): list user groups in profile and show button for admins to manage their user groups

---
+++ src/lib/components/UserInvite.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { User } from '$lib/types/User';
+
+    export let user: User;
+
+    $: isUserAdmin = user.user_groups.some((group) => group.is_admin);
+</script>
+
+isAdmin: {isUserAdmin}

7794f225107761fc1f36ef4039ed6b9380d527dd
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2025-12-19 12:46:47 -0500

    fix: remove schedule page poc and replace homepage hero link with ingestion status

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -6,27 +6,18 @@
 </script>
 
 <!-- Hero -->
-<div
-    class="hero min-h-[50vh] flex-grow"
-    style="background-image: url(/assets/img/custom/bli_grc_sm.jpg);"
->
+<div class="hero min-h-[50vh] flex-grow" style="background-image: url(/assets/img/custom/bli_grc_sm.jpg);">
     <div class="hero-overlay"></div>
     <div class="hero-content text-neutral-content text-center">
         <div class="max-w-xl">
             <h1 class="mb-5 text-5xl font-bold">Welcome to ACROSS</h1>
             <p class="mb-5 max-w-lg mx-auto">
-                The <strong class="text-white"
-                    >Astrophysics Cross-Observatory Science Support (ACROSS)</strong
-                > system focuses on the aggregation of NASA observatory state and
-                status information to facilitate science situational awareness.
+                The <strong class="text-white">Astrophysics Cross-Observatory Science Support (ACROSS)</strong> system focuses on the aggregation of NASA observatory
+                state and status information to facilitate science situational awareness.
             </p>
             <div class="flex flex-col md:flex-row md:gap-4">
-                <ArrowButton href={resolve('/schedules')}>
-                    View Schedules
-                </ArrowButton>
-                <ArrowButton href={resolve('/observations')}>
-                    Browse Observations
-                </ArrowButton>
+                <ArrowButton href={resolve('/ingestion-status')}>Data Ingestion Status</ArrowButton>
+                <ArrowButton href={resolve('/observations')}>Browse Observations</ArrowButton>
                 <ArrowButton href={API_URL + '/docs'}>Explore API</ArrowButton>
             </div>
         </div>

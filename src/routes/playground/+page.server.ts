9c3a8150f686d201b97750d9177d4fbb10d738d5
Nitzan Frock <nitzan.n.frock@nasa.gov>
2026-02-10 09:50:38 -0800

    fix: rm 404 message since it will be a default msg later

--- src/routes/playground/+page.server.ts
+++ src/routes/playground/+page.server.ts
@@ -3,7 +3,7 @@ import { PUBLIC_CONFIG } from '$config/config.public';
 
 export const load = () => {
     if (PUBLIC_CONFIG.BUILD_VERSION !== 'local') {
-        throw error(404, 'Sorry, the page you are looking for does not exist.');
+        throw error(404);
     }
 
     return {};

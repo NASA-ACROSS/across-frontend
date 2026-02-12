0fe5a6510e1675f0cef93245cd25b0cc6185ca76
Nitzan Frock <33351056+nitzan-frock@users.noreply.github.com>
2026-02-12 12:05:10 -0800

    feat(multiselect): add count and add component playground for testing (#220)
    * feat(multiselect): add count and add component playground for testing
    
    * fix: rm 404 message since it will be a default msg later
    
    * fix: change env to local
    
    * fix: update docs

---
+++ src/routes/playground/+page.server.ts
@@ -0,0 +1,10 @@
+import { error } from '@sveltejs/kit';
+import { PUBLIC_CONFIG } from '$config/config.public';
+
+export const load = () => {
+    if (PUBLIC_CONFIG.BUILD_VERSION !== 'local') {
+        throw error(404);
+    }
+
+    return {};
+};

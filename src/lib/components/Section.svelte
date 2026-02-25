ae8d7cb5192a9e96c316a0eac9645db4121461e8
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2026-02-25 11:37:54 -0500

    fix(layout): section component sets width to fill page component consistently (#249)

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -5,7 +5,7 @@
     export let containerClasses: string | undefined = '';
 </script>
 
-<div class="lg:mx-13 mx-3 mt-8 flex flex-col items-center {parentContainerClasses}">
+<div class="lg:mx-13 lg:w-256 w-full mx-3 mt-8 flex flex-col items-center {parentContainerClasses}">
     <!-- Section -->
     <div class="text-primary grow w-full {containerClasses}">
         <h2 class="text-3xl flex flex-col md:flex-row justify-between text-primary md:items-center items-start pb-3">

713dfc58e4cb9254a7fa786b4dd7f393a7532c99
ShaneCray <shanecray5@gmail.com>
2026-05-29 14:15:08 -0400

    feat: conditionally load USGOVAnalytics script based on production environment

--- src/lib/components/USGOVAnalytics.svelte
+++ src/lib/components/USGOVAnalytics.svelte
@@ -1,9 +1,15 @@
+<script lang="ts">
+    import { PUBLIC_CONFIG } from '$config/config.public';
+</script>
+
 <svelte:head>
-    <!-- We participate in the US government's analytics program. See the data at analytics.usa.gov. -->
-    <script
-        async
-        type="text/javascript"
-        src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=NASA"
-        id="_fed_an_ua_tag"
-    ></script>
+    {#if PUBLIC_CONFIG.IS_PROD}
+        <!-- We participate in the US government's analytics program. See the data at analytics.usa.gov. -->
+        <script
+            async
+            type="text/javascript"
+            src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=NASA"
+            id="_fed_an_ua_tag"
+        ></script>
+    {/if}
 </svelte:head>

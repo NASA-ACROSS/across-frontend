0f80cebd685567fbefd1864079912920b66e07ff
Kirill Vorobyev <35638949+Kirill-Vorobyev@users.noreply.github.com>
2026-07-08 14:43:24 -0400

    feat: observation request create page

---
+++ src/routes/observation-request/create/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import CoordinateSearch from '$lib/components/CoordinateSearch.svelte';
+    import Fieldset from '$lib/components/Fieldset.svelte';
+    import ObjectNameResolver from '$lib/components/ObjectNameResolver.svelte';
+    import Page from '$lib/components/Page.svelte';
+    import Section from '$lib/components/Section.svelte';
+</script>
+
+<Page title="Target of Opportunity Observation Request" icon="crosshair">
+    <Section>
+        <Fieldset title="Object Information">
+            <CoordinateSearch></CoordinateSearch>
+        </Fieldset>
+    </Section>
+</Page>

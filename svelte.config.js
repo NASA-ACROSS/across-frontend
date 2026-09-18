1823d88ae81a45a8c79fe4ac4f658de027ea05c6
Nitzan Frock <nitzan.n.frock@nasa.gov>
2026-09-18 15:53:46 -0700

    fix: set base path for rev proxy

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+try {
+    process.loadEnvFile('.env');
+    console.log('BASE_PATH:', process.env.BASE_PATH);
+} catch (err) {
+    console.warn('Warning: No .env file found', err);
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -13,6 +20,7 @@ const config = {
         },
         paths: {
             relative: false,
+            base: process.env.PUBLIC_BASE_PATH || '',
         },
 
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
